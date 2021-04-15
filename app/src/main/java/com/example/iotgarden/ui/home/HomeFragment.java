1339a717824a9d1eff63b0f192ccdcfbdd03e235
EthanDarling8 <edarling90@gmail.com>
2021-04-14 20:01:41 -0600

    7 day chart and refactoring

--- app/src/main/java/com/example/iotgarden/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/iotgarden/ui/home/HomeFragment.java
@@ -30,6 +30,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -40,97 +42,77 @@ public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private final String TAG = "Home Fragment";
+
     private DatabaseReference mDatabase;
+    private LineChart weekChart;
+
+    private LocalDate today = LocalDate.now();
+    private LocalDate pastSeven = today.minusDays(7);
+
+    private SoilReading sr;
+    private Object[] srKeyArray;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        LineChart chart = root.findViewById(R.id.chart);
-        TextView stemmaOne = root.findViewById(R.id.chartTitle);
+
+        LineChart dayChart = root.findViewById(R.id.dayChart);
+        weekChart = root.findViewById(R.id.weekChart);
+        TextView stemmaOne = root.findViewById(R.id.dayChartTitle);
 
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
             public void onChanged(@Nullable String s) {
                 mDatabase = FirebaseDatabase.getInstance().getReference();
-                LocalDate today = LocalDate.now();
-                LocalDate pastSeven = today.minusDays(7);
 
                 // Read from the database
                 ValueEventListener soilListener = new ValueEventListener() {
                     @RequiresApi(api = Build.VERSION_CODES.N)
                     @Override
                     public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        SoilReading sr = snapshot.getValue(SoilReading.class);
+                        sr = snapshot.getValue(SoilReading.class);
                         assert sr != null;
 
                         Object[] srKeyArray = sr.stemma_1.keySet().toArray();
                         Arrays.sort(srKeyArray);
-                        List<Entry> entries = new ArrayList<>();
+                        List<Entry> dayEntries = new ArrayList<>();
+                        List<Entry> weekEntries = new ArrayList<>();
 
-                        // Create custom chart axes
-                        createAxes(chart);
+                        // Create custom chart axes for dayChart
+                        createAxes(dayChart, true, 0, true, 24);
+                        createAxes(weekChart, true, pastSeven.getDayOfMonth(),
+                                true, today.minusDays(1).getDayOfMonth());
 
-                        // Populate entry list
-                        Reading value = null;
-                        value = populateEntryList(sr, srKeyArray, entries, value, today);
+                        // Populate entry list for dayChart
+                        Reading dayValue = null;
+                        dayValue = populateDayEntryList(sr, srKeyArray, dayEntries, dayValue, today);
 
-                        // Set chart title text
-                        assert value != null;
-                        stemmaOne.setText(value.name);
+                        // Populate entry list for weekChart
+                        populateWeekEntryList(srKeyArray, weekEntries);
 
                         // Sort entries by date
-                        entries.sort((e1, e2) -> Float.compare(e1.getX(), e2.getX()));
+                        dayEntries.sort((e1, e2) -> Float.compare(e1.getX(), e2.getX()));
+                        weekEntries.sort((e1, e2) -> Float.compare(e1.getX(), e2.getX()));
 
                         // Find 7 day min and max
-                        int min = 0;
-                        int max = 0;
-                        for (Object k : srKeyArray) {
-                            value = sr.stemma_1.get(k);
-                            assert value != null;
-                            int moisture = Integer.parseInt(value.soil.moisture);
-
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                                LocalDate valueDate = LocalDate.of(
-                                        Integer.parseInt(value.date.year),
-                                        Integer.parseInt(value.date.month),
-                                        Integer.parseInt(value.date.day));
-
-                                // If the values date is after today's date minus 7 days.
-                                if (valueDate.isAfter(today.minusDays(7))) {
-                                    if (min == 0 && max == 0) {
-                                        min = moisture;
-                                        max = moisture;
-                                    } else if (moisture < min) {
-                                        min = moisture;
-                                    } else if (moisture > min && moisture > max) {
-                                        max = moisture;
-                                    }
-                                }
-                            }
-                        }
-
-                        // Set min max text
-                        TextView minMaxTxt = root.findViewById(R.id.minMaxTxt);
-                        minMaxTxt.setText(String.format(Locale.US, "7 Day Min/Max: %d/%d", min, max));
+                        sevenDayMinMax(srKeyArray, root);
 
                         // Set recent reading text
                         TextView recentReading = root.findViewById(R.id.recentReading);
                         recentReading.setText(String.format(Locale.US,
                                 "Current: %.0f",
-                                entries.get(entries.size() - 1).getY()));
+                                dayEntries.get(dayEntries.size() - 1).getY()));
 
-                        // Create data set
-                        LineDataSet dataSet = new LineDataSet(entries, "Moisture");
-                        dataSet.setDrawCircles(false);
-                        dataSet.setLineWidth(4f);
-                        dataSet.setValueTextSize(9f);
-                        dataSet.setDrawValues(false);
+                        // Create data set for dayChart
+                        LineData dayLineData = createDataSet(dayEntries, "Moisture");
 
-                        LineData lineData = new LineData(dataSet);
+                        // Create data set for weekChart
+                        LineData weekLineData = createDataSet(weekEntries, "Moisture");
 
-                        // Create chart and set selection text
-                        chart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+                        // Create dayChart and set selection text
+                        dayChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
                             final TextView valueText = root.findViewById(R.id.valueText);
 
                             @Override
@@ -145,11 +127,33 @@ public class HomeFragment extends Fragment {
                                 valueText.setText(R.string.valueDetails);
                             }
                         });
-                        chart.setTouchEnabled(true);
-                        chart.getAxisRight().setEnabled(false);
-                        chart.setData(lineData);
-                        chart.getDescription().setEnabled(false);
-                        chart.invalidate(); // refresh
+                        dayChart.setTouchEnabled(true);
+                        dayChart.getAxisRight().setEnabled(false);
+                        dayChart.setData(dayLineData);
+                        dayChart.getDescription().setEnabled(false);
+                        dayChart.invalidate(); // refresh
+
+                        // Create weekChart and set selection text
+                        weekChart.setOnChartValueSelectedListener(new OnChartValueSelectedListener() {
+                            final TextView valueText = root.findViewById(R.id.valueText);
+
+                            @Override
+                            public void onValueSelected(Entry e, Highlight h) {
+                                valueText.setText(String.format(Locale.US,
+                                        "Selected: Day: %.0f Moisture: %.0f",
+                                        e.getX(), e.getY()));
+                            }
+
+                            @Override
+                            public void onNothingSelected() {
+                                valueText.setText(R.string.valueDetails);
+                            }
+                        });
+                        weekChart.setTouchEnabled(true);
+                        weekChart.getAxisRight().setEnabled(false);
+                        weekChart.setData(weekLineData);
+                        weekChart.getDescription().setEnabled(false);
+                        weekChart.invalidate(); // refresh
                     }
 
                     @Override
@@ -164,14 +168,107 @@ public class HomeFragment extends Fragment {
     }
 
     /**
+     * Finds the seven day minimum and maximum for the past seven days.
+     *
+     * @param srKeyArray Object[] Array of all the soil reading keys
+     * @param root View for the HomeFragment
+     */
+    private void sevenDayMinMax(Object[] srKeyArray, View root) {
+        Reading dayValue;
+        int min = 0;
+        int max = 0;
+        for (Object k : srKeyArray) {
+            dayValue = sr.stemma_1.get(k);
+            assert dayValue != null;
+            int moisture = Integer.parseInt(dayValue.soil.moisture);
+
+            LocalDate valueDate = LocalDate.of(
+                    Integer.parseInt(dayValue.date.year),
+                    Integer.parseInt(dayValue.date.month),
+                    Integer.parseInt(dayValue.date.day));
+
+            // If the values date is after today's date minus 7 days.
+            if (valueDate.isAfter(today.minusDays(7))) {
+                if (min == 0 && max == 0) {
+                    min = moisture;
+                    max = moisture;
+                } else if (moisture < min) {
+                    min = moisture;
+                } else if (moisture > min && moisture > max) {
+                    max = moisture;
+                }
+            }
+
+            // Set min max text
+            TextView minMaxTxt = root.findViewById(R.id.minMaxTxt);
+            minMaxTxt.setText(String.format(Locale.US, "7 Day Min/Max: %d/%d", min, max));
+        }
+    }
+
+    /**
+     * Populates the entry list from the firebase database. The entry list is used to create the
+     * data set for the line chart.
+     *
+     * @param srKeyArray Object[] Array of all the soil reading keys
+     * @param weekEntries List<Entry>  List of entries for the week
+     */
+    private void populateWeekEntryList(Object[] srKeyArray, List<Entry> weekEntries) {
+        Reading weekValue;
+        for (Object k : srKeyArray) {
+            weekValue = sr.stemma_1.get(k);
+            int valueDayInt = Integer.parseInt(weekValue.date.day);
+            int x, y;
+            int increment = 1;
+
+            // If the day of the month is between 1 and 7
+            while (increment <= 7) {
+                    y = getDayAverage(sr, srKeyArray, valueDayInt);
+                    x = valueDayInt;
+                    increment++;
+
+                if (!weekEntries.contains(new Entry(x, y))) {
+                    weekEntries.add(new Entry(x, y));
+                }
+            }
+        }
+    }
+
+    /**
+     * Create the set of data for the chart based off of the data entries
+     *
+     * @param entries List<Entry> List of entries
+     * @param label String
+     * @return LineData
+     */
+    @NotNull
+    private LineData createDataSet(List<Entry> entries, String label) {
+        LineDataSet dataSet = new LineDataSet(entries, label);
+        dataSet.setDrawCircles(false);
+        dataSet.setLineWidth(4f);
+        dataSet.setValueTextSize(9f);
+        dataSet.setDrawValues(false);
+        return new LineData(dataSet);
+    }
+
+    /**
      * Creates the X and Y axes for the Line chart
+     *
      * @param chart LineChart
      */
-    private void createAxes(LineChart chart) {
+    private void createAxes(LineChart chart, boolean setMin, int minInt, boolean setMax, int maxInt) {
         // Create X Axis
         XAxis xAxis = chart.getXAxis();
-        xAxis.setAxisMinimum(0);
-        xAxis.setAxisMaximum(24);
+
+        if (setMin)
+            xAxis.setAxisMinimum(minInt);
+        else
+            xAxis.resetAxisMinimum();
+
+        if (setMax)
+            xAxis.setAxisMaximum(maxInt);
+        else
+            xAxis.resetAxisMinimum();
+
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
         xAxis.setDrawLabels(true);
 
@@ -192,7 +289,7 @@ public class HomeFragment extends Fragment {
      * @param today      LocalDate of today's date
      * @return Reading
      */
-    private Reading populateEntryList(SoilReading sr, Object[] srKeyArray, List<Entry> entries, Reading value, LocalDate today) {
+    private Reading populateDayEntryList(SoilReading sr, Object[] srKeyArray, List<Entry> entries, Reading value, LocalDate today) {
         for (Object k : srKeyArray) {
             value = sr.stemma_1.get(k);
             String valueDate = value.date.year + value.date.month + value.date.day;
@@ -202,16 +299,89 @@ public class HomeFragment extends Fragment {
                 float tempX = Float.parseFloat(value.date.hour + value.date.minute) / 100;
 
                 // Normalize time data
-                float x = 0f;
-                if (tempX % 1 >= 0.5) {
-                    x = (float) Math.ceil(tempX);
-                } else if (tempX % 1 < 0.5) {
-                    x = (float) Math.floor(tempX);
-                } else if (tempX % 1 == 0) {
-                    x = tempX;
-                }
+                float x = normalizeData(tempX);
 
                 float y = Float.parseFloat(value.soil.moisture);
+
+                entries.add(new Entry(x, y));
+            }
+        }
+        return value;
+    }
+
+    /**
+     * Normalizes the x coordinate data for the dayChart.
+     *
+     * @param tempX float that represents a temporary x coordinate
+     * @return float
+     */
+    private float normalizeData(float tempX) {
+        float x = 0f;
+        if (tempX % 1 >= 0.5) {
+            x = (float) Math.ceil(tempX);
+        } else if (tempX % 1 < 0.5) {
+            x = (float) Math.floor(tempX);
+        } else if (tempX % 1 == 0) {
+            x = tempX;
+        }
+        return x;
+    }
+
+    /**
+     * Gets the average moisture for a given day of the month.
+     *
+     * @param sr SoilReading Hash Map from databaseg
+     * @param srKeyArray Object[] Array of all the soil reading keys
+     * @param day int the given day of the month
+     * @return int
+     */
+    private int getDayAverage(SoilReading sr, Object[] srKeyArray, int day) {
+        Reading value;
+        int avg = 0;
+        int valueNumber = 0;
+
+        for (Object k : srKeyArray) {
+            value = sr.stemma_1.get(k);
+
+            int valueDayInt = Integer.parseInt(value.date.day);
+            if (valueDayInt == day) {
+                valueNumber++;
+                avg += Integer.parseInt(value.soil.moisture);
+            }
+        }
+        return avg / valueNumber;
+    }
+
+    /**
+     * Populates the entry list from the firebase database. The entry list is used to create the
+     * data set for the line chart.
+     *
+     * @param sr         SoilReading Hash Map from database
+     * @param srKeyArray Object[] Array of all the soil reading keys
+     * @param entries    List<Entry>  List of entries
+     * @param value      Reading that contains the name, date, and soil information.
+     * @param today      LocalDate of today's date
+     * @return Reading
+     */
+    private Reading populateWeekEntryList(SoilReading sr, Object[] srKeyArray, List<Entry> entries, Reading value, LocalDate today) {
+        for (Object k : srKeyArray) {
+            value = sr.stemma_1.get(k);
+
+            int valueDayInt = Integer.parseInt(value.date.day);
+
+            LocalDate valueDate = LocalDate.of(
+                    Integer.parseInt(value.date.year),
+                    Integer.parseInt(value.date.month),
+                    valueDayInt);
+
+            if (valueDate.isAfter(today.minusDays(8)) || valueDate.isBefore(today)) {
+                int sum = 0;
+                float tempX = 0f, x = 0f;
+
+                int y = getDayAverage(sr, srKeyArray, valueDayInt);
+
+                // Normalize time data
+                x = normalizeData(tempX);
 
                 entries.add(new Entry(x, y));
             }
